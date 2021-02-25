ebf9e8c136271a520fff7dc8f28a57f8879d3e82
Emmanuel Aboah Boateng <aboahemmanuel01@yahoo.com>
2021-02-25 00:55:16 -0600

    Update traffic_light.c

--- Traffic_light_intersection/traffic_light.c
+++ Traffic_light_intersection/traffic_light.c
@@ -21,20 +21,20 @@ _CONFIG4(DSWDTPS_DSWDTPS3 & DSWDTOSC_LPRC & RTCOSC_LPRC & DSBOREN_OFF & DSWDTEN_
 
 
 // Define delays of the various intersections
-#define M_Delay 65000      /* Main road delay on green for normal hours */
-#define S_Delay 55000      /* Small road delay on green for normal hours */
-#define Y_Delay 45000      /* Yellow light delay for normal hours */
-#define OM_Delay 50000      /* Main road delay on green for office hours */
-#define OS_Delay 3000       /* Small road delay on green for office hours */
-#define OY_Delay 25000       /* Yellow light delay on green for office hours */
-#define State_1 0X208C        /* Logic for state 1 */
-#define Y_State_1 0X2092      /* Logic for yellow state 1 */
-#define State_2 0X20A1         /* Logic for state 2 */
-#define Y_State_2 0X2111      /* Logic for yellow state 2 */
-#define State_3 0X2209         /* Logic for state 3 */
-#define Y_State_3 0X4109      /* Logic for yellow state 3 */
-#define State_4 0X8089         /* Logic for state 4 */
-#define Y_State_4 0X408A        /* Logic for yellow state 4 */
+#define M_Delay 65000        // Main road delay on green for normal hours 
+#define S_Delay 55000        // Small road delay on green for normal hours 
+#define Y_Delay 45000        // Yellow light delay for normal hours 
+#define OM_Delay 50000       // Main road delay on green for office hours 
+#define OS_Delay 3000        // Small road delay on green for office hours 
+#define OY_Delay 25000       // Yellow light delay on green for office hours 
+#define State_1 0X208C       // Logic for state 1 
+#define Y_State_1 0X2092     // Logic for yellow state 1 
+#define State_2 0X20A1       // Logic for state 2 
+#define Y_State_2 0X2111     // Logic for yellow state 2 
+#define State_3 0X2209       // Logic for state 3 
+#define Y_State_3 0X4109     // Logic for yellow state 3 
+#define State_4 0X8089       // Logic for state 4 
+#define Y_State_4 0X408A     // Logic for yellow state 4 
 #include "xc.h"
 
 // Main program
@@ -45,7 +45,7 @@ int main(int argc, char**argv)
 TRISB = 0x000; T1CON = 0X8030; 
 while (1) {
 
-for (k = 0; a < 20  k++)       /* repeat cycle for 20 sequence  for office period*/
+for (k = 0; a < 20  k++)       // repeat cycle for 20 sequence  for office period
 
 {
 
@@ -82,8 +82,7 @@ while (TMR1<OY_Delay) {}
 
 }  end for
 
-For (k = 0; a < 40 k++)        /* repeat cycle for 40 sequences for normal running period*/
-
+For (k = 0; a < 40 k++)        // repeat cycle for 40 sequences for normal running period
 { LATB = State_1; TMR1 = 0;
 while (TMR1<M_Delay) {} LATB = Y_State_1;
 TMR1 = 0;
